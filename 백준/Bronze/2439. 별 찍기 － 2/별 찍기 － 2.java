dff09c17ac389efa03aac4c5f8a4791be4250a1e
YunJiYoung <jjiyoung0421@gmail.com>
2025-03-11 19:31:45 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 296 ms, Memory: 22420 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //case n 받기
+        //공백 (n - i)개 * i개 출력
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1 ; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
