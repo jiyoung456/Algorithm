44285ece98aecc5f5963b14f404914bc7de147c6
Jiyoung Yun <jjiyoung0421@gmail.com>
2025-11-04 19:35:07 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16216 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(num + " * " + i + " = " + (num * i));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
         }
     }
 }
-
